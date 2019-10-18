8b7a6842cac44657f9bf0d573dcf9a4894349db4
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-18 10:27:33 -0700

    added a question box/ object to pull questions from

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,43 @@
 $(document).ready(function () {
-    var time = 5;
+    const questionsBox = [
+        {
+            question: "Cruella de Vil is the villain in which movie?",
+            choices: {
+                a: "Sleeping Beauty",
+                b: "101 Dalmations",
+                c: "Oliver and Company"
+            },
+            answer: "b"
+        },
+        {
+            question: "In Finding Nemo, which country has Nemo been taken to?",
+            choices: {
+                a: "Austrailia",
+                b: "Italy",
+                c: "Germany"
+            },
+            answer: "a"
+        },
+        {
+            question: "In Peter Pan, what did the crocodile swallow?",
+            choices: {
+                a: "A Hook",
+                b: "Smee",
+                c: "A clock"
+            },
+            answer: "c"
+        },
+        {
+            question: "Which movie has the quote 'How does the smiling Crocodile improve his shining tail?'",
+            choices: {
+                a: "Alice in Wonderland",
+                b: "Mary Poppins",
+                c: ""
+            },
+            answer: "a"
+        }]
+
+    var time = 30;
 
     var button = $('.btn');
     var container = $('.container');
@@ -10,14 +48,22 @@ $(document).ready(function () {
     button.on('click', function () {
         button.remove();
         container.append(timer);
+        play();
+    });
+
+    function play() {
         var countDown = setInterval(function () {
             time--;
             timer.text(time);
             if (time <= 0) {
                 clearInterval(countDown);
+                endRound();
             }
         }, 1000);
 
-    });
+    }
+    function endRound() {
+
+    }
 
 });
